0c9e0ba29201dc1250f8a10a068a1dbd904f1f30
Eduardo Medina Medina <46487685+Marcuss17@users.noreply.github.com>
2019-09-03 13:43:16 -0500

    Update main.cpp
    Included the asserts for every method of the class.

--- main.cpp
+++ main.cpp
@@ -8,36 +8,57 @@ using std::endl;
 
 int main() {
     srand(time(NULL));
-    int rows, columns;
-    cout <<"Enter data from the first Matrix: " << endl;
+    /* For the asserts
+     * m1 = [1 2;3 4] ; means change of row
+     * m2 = [1 2; 3 4]
+     * Choose fill option 1 in both matrix*/
+    int rows, columns, fillOption;
+    cout <<"Choose fill option, press 0 if you want random numbers in the matrix or 1 to input: \n";
+    cin >> fillOption;
+
+    cout <<"Enter data for the first Matrix: " << endl;
     cout <<"Number of rows: "; cin >> rows;
     cout <<"Number of columns: "; cin >> columns;
     Matrix m1(rows,columns);
-    m1.fillMatrix();
-    cout <<"Printing Matrix: " << endl;
-    cout <<"Matrix1: " << endl;
-    m1.showMatrix();
-    cout <<"Enter data from a second Matrix: " << endl;
+    m1.fillMatrix(fillOption);
+
+    cout <<"Enter data for a second Matrix: " << endl;
     cout <<"Number of rows: "; cin >> rows;
     cout <<"Number of columns "; cin >> columns;
     Matrix m2(rows,columns);
-    m2.fillMatrix();
-    cout <<"Matrix2: " << endl;
-    m2.showMatrix();
-    cout <<"Matrix sum: " << endl;
-    Matrix m3 = m1+m2;
-    /*assert(m3.mMatrix[0][0]==6);
-    m3.showMatrix();
-    cout << endl;
-    Matrix m4 = m1*m2;
-    cout <<"Matrix multiplication: " << endl;
-    m4.showMatrix();
-    Matrix m5 = m1*5;
-    m5.showMatrix();
-    cout <<"Transpose Matrix: "  << endl;
-    m5.transpose();
-    m5.showMatrix();*/
+    m2.fillMatrix(fillOption);
 
+    //Matrix sum assert
+    Matrix m3 = m1+m2;
+    assert(m3[1][0]==6);
+    //
+
+    //Matrix scalar multiplication
+    Matrix m4 = m3*2;
+    assert(m4[0][1] == 8);
+    //
+
+    //Matrix multiplication
+    Matrix m5 = m1*m2;
+    assert(m5[0][0] == 7);
+    //
+
+    //Matrix transpose
+    m5.transpose();
+    assert(m5[1][1] == 22);
+    //
+
+    cout <<"Printing all the matrixes: \n";
+    cout <<"Matrix 1: \n";
+    m1.showMatrix();
+    cout <<"Matrix 2: \n";
+    m2.showMatrix();
+    cout <<"Matrix 3: \n";
+    m3.showMatrix();
+    cout <<"Matrix 4: \n";
+    m4.showMatrix();
+    cout <<"Matrix 5: \n";
+    m5.showMatrix();
 
     return 0;
 }
